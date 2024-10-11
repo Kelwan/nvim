9950f927ec9b87f28ebcc8283d0dd77733016df4
Kelwan <austin@kelway.ca>
2024-10-11 09:00:30 -0700

    Fix keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-<<<<<<< Updated upstream
 local map = vim.keymap
 map.del("n", "<leader>gG")
-=======
 
 vim.keymap.delete("n", "<leader>ub")
 vim.keymap.delete("n", "<leader>uB")
@@ -12,4 +10,3 @@ vim.keymap.delete("n", "<leader>uP")
 vim.keymap.delete("n", "<leader>uo")
 vim.keymap.delete("n", "<leader>ur")
 vim.keymap.delete("n", "<leader>uc")
->>>>>>> Stashed changes
