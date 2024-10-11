5917cc14fec3f65c61b006d35e2546292c0dd5ed
Kelwan <austin@kelway.ca>
2024-10-11 08:58:02 -0700

    Unreal engine commands and LF config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+<<<<<<< Updated upstream
 local map = vim.keymap
 map.del("n", "<leader>gG")
+=======
+
+vim.keymap.delete("n", "<leader>ub")
+vim.keymap.delete("n", "<leader>uB")
+vim.keymap.delete("n", "<leader>up")
+vim.keymap.delete("n", "<leader>uP")
+vim.keymap.delete("n", "<leader>uo")
+vim.keymap.delete("n", "<leader>ur")
+vim.keymap.delete("n", "<leader>uc")
+>>>>>>> Stashed changes
