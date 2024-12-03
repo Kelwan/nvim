f05a987e45dffc1d7f445ef734d6991975d0a342
Kelwan <austin@kelway.ca>
2024-12-03 09:52:47 -0800

    Titlestring for Linux/Windows

--- lua/kelwan/init.lua
+++ lua/kelwan/init.lua
@@ -4,4 +4,15 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 vim.opt.relativenumber = true
-vim.opt.number = true
+
+vim.opt.title = true
+
+local _, _, wslenv_err = vim.uv.os_getenv("WSLENV", 1)
+if wslenv_err ~= nil and wslenv_err == "ENOENT" then
+	vim.g.wslenv = false
+	vim.opt.titlestring = "NeovideWin"
+else
+	vim.g.wslenv = true
+	vim.opt.titlestring = "NeovideLin"
+	endim.opt.number = true
+end
